edc8ca3de13c5ee1e40ae4dc7a9a03e3fbe2ffcf
Akselinc <akselinc.dev@gmail.com>
2023-11-05 11:31:57 +0100

    update

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://doktor.akselinc.com',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+}
